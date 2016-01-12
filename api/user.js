aa8d109fbde30eff2756bea67248e92ec12f7bd4
byronvis <elvis07@163.com>
2016-01-12 09:59:45 +0800

    修复bug

--- api/user.js
+++ api/user.js
@@ -129,12 +129,10 @@ module.exports = {
   },
   getUserHome: function(req, res, next) {
 
-    var user = {
-      username: req.session.user.username
-    };
+
     res.render('user/u', {
       title: req.params.id,
-      user: req.params.id === user.username ? user : null
+      user: req.session.user && req.params.id === req.session.user.username ? req.session.user : null
     });
   }
 
