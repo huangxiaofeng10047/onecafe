52b9c9c6988065bcc968790da4c855cde4e203b6
Byronvis <elvis07@163.com>
2016-03-30 20:02:47 +0800

    del question

--- public/assets/js/question.js
+++ public/assets/js/question.js
@@ -58,6 +58,24 @@
 	  $(this).text(moment(time).format("YYYY-MM-DD"));
 	});
 
+	/**
+	 * 删除问题
+	 */
+
+	$('.del_btn').click(function () {
+	  $.ajax({
+	    "url": '/q/'+$(this).data('question'),
+	    "method": 'delete',
+	    "dataType": 'json'
+	  }).done(function (data) {
+	    if(data.success){
+	      window.location.href='/';
+	    }else {
+	      alert(data.message);
+	    }
+	  });
+	});
+
 
 /***/ },
 /* 1 */
--- public/src/js/question.js
+++ public/src/js/question.js
@@ -11,3 +11,21 @@ $('.time').each(function () {
   var time=$(this).data('create');
   $(this).text(moment(time).format("YYYY-MM-DD"));
 });
+
+/**
+ * 删除问题
+ */
+
+$('.del_btn').click(function () {
+  $.ajax({
+    "url": '/q/'+$(this).data('question'),
+    "method": 'delete',
+    "dataType": 'json'
+  }).done(function (data) {
+    if(data.success){
+      window.location.href='/';
+    }else {
+      alert(data.message);
+    }
+  });
+});
